0006625c4e7bd05286ca726e3ddd39e475c63f5a
Luis Eduardo Passoni <115907714+Dudu-Passoni@users.noreply.github.com>
2024-07-12 09:47:33 -0300

    fix: Fixed -h option for backup.sh

--- backup.sh
+++ backup.sh
@@ -5,7 +5,11 @@
 source_dir=$1
 backup_dir=$2
 
-if [ -z "$source_dir" ] && [ -z "$backup_dir" ]; then
+if [[ "$1" == "-h" ]]; then
+        echo "backup [source directory] [backup directory]"
+else
+
+if [[ -z "$source_dir" ]] && [[ -z "$backup_dir" ]]; then
         echo "Digite o caminho do arquivo/diretório para backup"
         read source_dir
 
@@ -19,3 +23,4 @@ filename=$(basename "$source_dir")
 backup_file="backup_${filename%.*}-$timestamp.tar.gz"
 
 tar -czvf "$backup_dir/$backup_file" "$source_dir"
+fi
