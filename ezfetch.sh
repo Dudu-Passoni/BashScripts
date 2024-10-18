faa6a346b7fb625330319ed1c451316bbf5fcf2e
Dudu-Passoni <dudupassoni@gmail.com>
2024-10-18 10:14:23 -0300

    feat:centos, rhel; yum and dnf control

--- ezfetch.sh
+++ ezfetch.sh
@@ -8,11 +8,14 @@ RED="\033[1;31m"
 ORANGE="\033[38;5;208m"
 LIGHT_GREEN="\033[1;92m"
 BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+
 
 logo_display() {
 if [ -f /etc/os-release ]; then
 	. /etc/os-release
 fi
+
 case $ID in
 
 	arch)
@@ -79,19 +82,62 @@ echo -e " '.     ,'"
 echo -e "   '''''"
 echo -e "${RESET}"
 COR=${BLUE}
-PACOTES=$(dnf list installed | wc -l)
+
+if [[ $VERSION_ID == 22 || $VERSION_ID > 22 ]]; then
+	PACOTES=$(dnf list installed | wc -l)
+else
+	PACOTES=$(yum list installed | wc -l)
+fi
 		;;
-
+	rhel)
+echo -e "${RED}            .MMM..:MMMMMMM"
+echo -e "          MMMMMMMMMMMMMMMMMM"
+echo -e "          MMMMMMMMMMMMMMMMMMMM."
+echo -e "         MMMMMMMMMMMMMMMMMMMMMM"
+echo -e "        ,MMMMMMMMMMMMMMMMMMMMMM:"
+echo -e "        MMMMMMMMMMMMMMMMMMMMMMMM"
+echo -e "  .MMMM'  MMMMMMMMMMMMMMMMMMMMMM"
+echo -e " MMMMMM    \MMMMMMMMMMMMMMMMMMMM."
+echo -e "MMMMMMMM      MMMMMMMMMMMMMMMMMM ."
+echo -e "MMMMMMMMM.       \MMMMMMMMMMMMM' MM."
+echo -e "MMMMMMMMMMM.                     MMMM"
+echo -e "\MMMMMMMMMMMMM.                 ,MMMMM."
+echo -e " \MMMMMMMMMMMMMMMMM.          ,MMMMMMMM."
+echo -e "    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+echo -e "      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM:"
+echo -e "         MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"
+echo -e "            \MMMMMMMMMMMMMMMMMMMMMMMM:"
+echo -e "                \MMMMMMMMMMMMMMMMM"
+echo -e "${RESET}"
+if [[ $VERSION_ID == 8 || $VERSION_ID > 8 ]]; then
+	PACOTES=$(dnf list installed | wc -l)
+else
+	PACOTES=$(yum list installed | wc -l)
+fi
+		;;
+	centos)
+echo -e "${PURPLE}____^____"
+echo -e "|\  |  /|"
+echo -e "| \ | / |"
+echo -e "<-------->"
+echo -e "| / | \ |"
+echo -e "|/__|__\|"
+echo -e "    v"
+echo -e "${RESET}"
+if [[ $VERSION_ID == 8 || $VERSION_ID > 8 ]]; then
+	PACOTES=$(dnf list installed | wc -l)
+else
+	PACOTES=$(yum list installed | wc -l)
+fi
+		;;
 esac
-
 }
+
 KERNEL_VERSION=$(uname -r)
 UPTIME=$(uptime -p)
 USER_NAME=$(whoami)
 HOSTNAME=$(hostname)
 SHELL_NAME=$(basename $SHELL)
-
-
 
 logo_display
 echo "$USER_NAME@$HOSTNAME"
@@ -101,8 +147,3 @@ echo "Kernel version: $KERNEL_VERSION"
 echo "Packages: $PACOTES"
 echo "Shell: $SHELL_NAME"
 echo "Terminal: $TERM"
-
-#echo "$UPTIME"
-
-
-
